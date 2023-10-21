10588e3ccf3c8127acc49f21c0e0c0f7ab9b35d9
Gláudia Almeida <137931174+glaudiaalmeida@users.noreply.github.com>
2023-10-21 19:15:56 -0300

    calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,24 @@
+def calculadora():
+ 
+    try:
+      num1 = int(input("insira o primeiro número inteiro: "))
+      num2 = int(input("insira o segundo número inteiro: "))
+      operador = int(input
+      ("Digite o número referente a operação que deseja realizar: (1. Soma; 2. Subtração; 3. Multiplicação; 4. Divisão) "))
+      if (operador == 1):
+        resultado = num1 + num2
+        print(num1, " + ", num2, " = ", resultado)
+      elif(operador == 2):
+        resultado = num1 - num2
+        print( num1, " - ", num2, " = ", resultado)
+      elif(operador == 3):
+        resultado = num1 * num2
+        print( num1, " * ", num2, " = ", resultado)4
+        
+      elif(operador == 4):
+        resultado = num1 / num2
+        print(num1, " / ", num2, " = ", resultado)
+    except:
+      print("Por favor digite um número válido!")
+
+calculadora()
